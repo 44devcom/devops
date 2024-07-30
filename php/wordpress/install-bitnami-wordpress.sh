96cc9e2d7bdf60da09943800629fea5cc4802431
Peter Wright <info@44dev.com>
2024-07-30 07:22:24 +0000

    Fix

--- php/wordpress/install-bitnami-wordpress.sh
+++ php/wordpress/install-bitnami-wordpress.sh
@@ -59,7 +59,7 @@ check_success "WP-CLI is not installed correctly."
 
 if [ "$FORCE" = true ]; then
         echo "Dropping existing WordPress database tables..."
-        wp db reset --yes --path=$WORDPRESS_PATH
+        wp db reset --path=$WORDPRESS_PATH --yes
         check_success "Failed to drop existing database tables."
     # Remove existing WordPress installation if it exists
     if [ -d "$WORDPRESS_PATH" ] && [ -n "$(ls -A $WORDPRESS_PATH)" ]; then
